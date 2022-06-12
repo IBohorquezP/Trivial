414c5c9bc99faf7344f81f8c116603025191d81c
Ismael Prado <pradoismael50@gmail.com>
2022-06-11 22:00:58 -0400

    add statics files & jinja2 dependency

--- user/controller.py
+++ user/controller.py
@@ -7,7 +7,22 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-
 @router.get("/")
-async def read_items():
+async def index_users():
     return User.index()
+
+@router.get("/{id}")
+async def read_user(id: str):
+    return User.show(id)
+
+@router.post("/")
+async def create_user(params):
+    return User.store(params)
+
+@router.put("/{id}")
+async def update_user(id, params):
+    return User.update(id, params)
+
+@router.delete("/{id}")
+async def delete_user(id):
+    return User.delete(id)
