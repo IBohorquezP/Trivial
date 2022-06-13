83c9d1d2ac717209ab3b03959a64c7156e57a12c
Ismael Prado <pradoismael50@gmail.com>
2022-06-12 21:20:55 -0400

    implementing FrontEnd

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
-from fastapi import FastAPI
-from user.controller import router as user
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from src.user.controller import router as user
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+
+app.mount("/css", StaticFiles(directory="static/css"), name="css")
+app.mount("/js", StaticFiles(directory="static/js"), name="js")
+app.mount("/img", StaticFiles(directory="static/img"), name="img")
+
+templates = Jinja2Templates(directory="templates")
+
 app.include_router(user)
+
+@app.get("/", response_class=HTMLResponse)
+async def read_item(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request, "id": 1})
