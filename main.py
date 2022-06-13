c7c6bd0fd0fbcacc753257b65b4db70f3dddbe67
chowanza <luiscandialesfajardo@gmail.com>
2022-06-13 13:36:19 -0400

    statistics and gameboard

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ async def read_item(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "id": 1})
 
 @app.get("/character", response_class=HTMLResponse)
-async def read_item(request: Request):
+async def character(request: Request):
     return templates.TemplateResponse("character.html", {"request": request, "id": 1})
 
 @app.get("/statistics", response_class=HTMLResponse)
@@ -28,3 +28,7 @@ async def statistics(request: Request):
 @app.get("/register", response_class=HTMLResponse)
 async def register(request: Request):
     return templates.TemplateResponse("register.html", {"request": request, "id": 1})
+
+@app.get("/gameboard", response_class=HTMLResponse)
+async def gameboard(request: Request):
+    return templates.TemplateResponse("gameboard.html", {"request": request, "id": 1})
