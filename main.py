a28d3d23a5e919f6e8f2667fa79b104cff3d6d6d
Ismael Prado <pradoismael50@gmail.com>
2022-06-13 00:41:49 -0400

    login implemented

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from src.user.controller import router as user
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,3 +19,4 @@ app.include_router(user)
 @app.get("/", response_class=HTMLResponse)
 async def read_item(request: Request):
     return templates.TemplateResponse("login.html", {"request": request, "id": 1})
+
--- src/user/controller.py
+++ src/user/controller.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Form, Request
 from .model import User
 
 router = APIRouter(
@@ -11,18 +11,23 @@ router = APIRouter(
 async def index_users():
     return User.index()
 
-@router.get("/{id}")
-async def read_user(id: str):
-    return User.show(id)
+# @router.get("/{id}")
+# async def read_user(id: str):
+#     return User.show(id)
 
-@router.post("/")
-async def create_user(params):
-    return User.store(params)
+# @router.post("/")
+# async def create_user(params):
+#     return User.store(params)
 
-@router.put("/{id}")
-async def update_user(id, params):
-    return User.update(id, params)
+# @router.put("/{id}")
+# async def update_user(id, params):
+#     return User.update(id, params)
 
-@router.delete("/{id}")
-async def delete_user(id):
-    return User.delete(id)
+# @router.delete("/{id}")
+# async def delete_user(id):
+#     return User.delete(id)
+
+@router.post("/login")
+async def login(username: str = Form(), password: str = Form()):
+    is_authenticated = User.login(username,password)
+    return {"se logro el login?": is_authenticated}
